f44b2040edc19949372866a083ffb1966d4db73d
Igor Dolganov <igor.dolganoff@gmail.com>
2024-10-20 11:44:40 +0500

    fix parametrize

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,91 +1,22 @@
-from conftest import setup_driver
 from locators.main_page_locators import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from tests.test_questions import *
+from conftest import setup_driver
 
 class MainPageQuestions:
 
     def __init__(self, setup_driver):
         self.driver = setup_driver
 
-    def click_question1(self):
+    def click_question(self, button):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_1_button).click()
+        self.driver.find_element(*button).click()
 
-    def click_question2(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_2_button).click()
+    def check_question(self, text_locator, expected):
+        actually_value = self.driver.find_element(*text_locator).text
+        assert actually_value == expected, f"неверный текст"
 
-    def click_question3(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_3_button).click()
-
-    def click_question4(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_4_button).click()
-
-    def click_question5(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_5_button).click()
-
-    def click_question6(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_6_button).click()
-
-    def click_question7(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_7_button).click()
-
-    def click_question8(self):
-        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        self.driver.find_element(*MainPage.question_8_button).click()
-
-    def check_question1(self):
-        actually_value = self.driver.find_element(*MainPage.question_1_text).text
-        expected_value = 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question2(self):
-        actually_value = self.driver.find_element(*MainPage.question_2_text).text
-        expected_value = ('Пока что у нас так: один заказ — один самокат.'
-                          ' Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.')
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question3(self):
-        actually_value = self.driver.find_element(*MainPage.question_3_text).text
-        expected_value = ('Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. '
-                          'Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. '
-                          'Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.')
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question4(self):
-        actually_value = self.driver.find_element(*MainPage.question_4_text).text
-        expected_value = 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question5(self):
-        actually_value = self.driver.find_element(*MainPage.question_5_text).text
-        expected_value = 'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.'
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question6(self):
-        actually_value = self.driver.find_element(*MainPage.question_6_text).text
-        expected_value = ('Самокат приезжает к вам с полной зарядкой. '
-                          'Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. '
-                          'Зарядка не понадобится.')
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question7(self):
-        actually_value = self.driver.find_element(*MainPage.question_7_text).text
-        expected_value = ('Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. '
-                          'Все же свои.')
-        assert actually_value == expected_value, "неверный текст"
-
-    def check_question8(self):
-        actually_value = self.driver.find_element(*MainPage.question_8_text).text
-        expected_value = 'Да, обязательно. Всем самокатов! И Москве, и Московской области.'
-        assert actually_value == expected_value, "неверный текст"
 
     def check_yandex_logo_button(self):
         self.driver.find_element(*MainPage.yandex_logo).click()
--- tests/test_questions.py
+++ tests/test_questions.py
@@ -3,49 +3,31 @@ import pytest
 
 class TestQuestions:
 
-    def test_question1(self, setup_driver):
-        question1 = MainPageQuestions(setup_driver)
-        question1.click_question1()
-        question1.check_question1()
-
-    def test_question2(self, setup_driver):
-
-        question2 = MainPageQuestions(setup_driver)
-        question2.click_question2()
-        question2.check_question2()
-
-    def test_question3(self, setup_driver):
-
-        question3 = MainPageQuestions(setup_driver)
-        question3.click_question3()
-        question3.check_question3()
-
-    def test_question4(self, setup_driver):
-
-        question4 = MainPageQuestions(setup_driver)
-        question4.click_question4()
-        question4.check_question4()
-
-    def test_question5(self, setup_driver):
-
-        question5 = MainPageQuestions(setup_driver)
-        question5.click_question5()
-        question5.check_question5()
-
-    def test_question6(self, setup_driver):
-
-        question6 = MainPageQuestions(setup_driver)
-        question6.click_question6()
-        question6.check_question6()
-
-    def test_question7(self, setup_driver):
-
-        question7 = MainPageQuestions(setup_driver)
-        question7.click_question7()
-        question7.check_question7()
-
-    def test_question8(self, setup_driver):
-
-        question8 = MainPageQuestions(setup_driver)
-        question8.click_question8()
-        question8.check_question8()
+    @pytest.mark.parametrize("button, text_locator, expected", [
+        (MainPage.question_1_button, MainPage.question_1_text,
+         'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'),
+        (MainPage.question_2_button, MainPage.question_2_text,
+         'Пока что у нас так: один заказ — один самокат.'
+          ' Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за '
+          'другим.'),
+        (MainPage.question_3_button, MainPage.question_3_text,
+         'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. '
+                          'Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. '
+                          'Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.'),
+        (MainPage.question_4_button, MainPage.question_4_text,
+         'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'),
+        (MainPage.question_5_button, MainPage.question_5_text,
+         'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.'),
+        (MainPage.question_6_button, MainPage.question_6_text,
+         'Самокат приезжает к вам с полной зарядкой. '
+         'Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.'),
+        (MainPage.question_7_button, MainPage.question_7_text,
+         'Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. '
+         'Все же свои.'),
+        (MainPage.question_8_button, MainPage.question_8_text,
+         'Да, обязательно. Всем самокатов! И Москве, и Московской области.')
+    ])
+    def test_questions(self, setup_driver, button, text_locator, expected):
+        question = MainPageQuestions(setup_driver)
+        question.click_question(button)
+        question.check_question(text_locator, expected)
